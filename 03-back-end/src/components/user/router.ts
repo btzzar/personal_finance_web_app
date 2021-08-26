486fd49cf122ae050b2ad6f962eb92a77e01750d
btzzar <luka.m.banovic@gmail.com>
2021-08-26 11:08:32 +0200

    Reorganize routes

--- 03-back-end/src/components/user/router.ts
+++ 03-back-end/src/components/user/router.ts
@@ -2,9 +2,10 @@ import * as express from "express";
 import UserService from "./service";
 import UserController from "./controller";
 import IApplicationResources from "../../common/IApplicationResources.interface";
+import IRouter from "../../common/IRouter.interface";
 
-export default class UserRouter {
-    public static setupRoutes(application: express.Application, resources: IApplicationResources){
+export default class UserRouter implements IRouter{
+    public setupRoutes(application: express.Application, resources: IApplicationResources){
         
         const userService = new UserService(resources.databaseConnection);
         const userController = new UserController(userService);
