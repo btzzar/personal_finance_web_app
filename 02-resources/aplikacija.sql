9d4a9819b548535d8a2d452de9c358ea800590f0
btzzar <luka.m.banovic@gmail.com>
2021-08-28 14:33:36 +0200

    Add sql dump and postman tests of all components

--- 02-resources/aplikacija.sql
+++ 02-resources/aplikacija.sql
@@ -16,13 +16,13 @@ CREATE TABLE IF NOT EXISTS `account` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `currency` enum('eur','rsd','usd','gbp') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'eur',
   `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `total` decimal(10,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`account_id`),
   UNIQUE KEY `uq_account_name` (`name`),
   KEY `fk_account_user_id` (`user_id`),
   CONSTRAINT `fk_account_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `expense`;
 CREATE TABLE IF NOT EXISTS `expense` (
@@ -30,12 +30,12 @@ CREATE TABLE IF NOT EXISTS `expense` (
   `account_id` int unsigned NOT NULL,
   `category` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `value` decimal(10,2) unsigned NOT NULL,
-  `currency` enum('eur','rsd','usd','gbp') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'eur',
+  `currency` enum('eur','rsd','usd','gbp') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'eur',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`expense_id`),
   KEY `fk_expense_account_id` (`account_id`),
   CONSTRAINT `fk_expense_account_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `income`;
 CREATE TABLE IF NOT EXISTS `income` (
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `income` (
   PRIMARY KEY (`income_id`),
   KEY `fk_income_account_id` (`account_id`),
   CONSTRAINT `fk_income_account_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
@@ -62,7 +62,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `uq_user_username` (`username`),
   UNIQUE KEY `uq_user_email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TRIGGER IF EXISTS `tr_account_update_total_after_expense`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `tr_account_update_total_after_expense` AFTER INSERT ON `expense` FOR EACH ROW UPDATE `account` SET total = total - NEW.value WHERE NEW.account_id = account_id//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+DROP TRIGGER IF EXISTS `tr_account_update_total_after_income`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `tr_account_update_total_after_income` AFTER INSERT ON `income` FOR EACH ROW UPDATE `account` SET total = total + NEW.value WHERE NEW.account_id = account_id//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
