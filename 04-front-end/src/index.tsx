64d79faeac583e05d58914f3333b83e6575ba882
btzzar <luka.m.banovic@gmail.com>
2021-09-02 21:07:21 +0200

    Init react project for front-end

---
+++ 04-front-end/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Application from './components/Application/Application';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
