7a6d0b5670735a08b260218234d5e657c017759e
btzzar <luka.m.banovic@gmail.com>
2021-09-02 20:35:58 +0200

    Refine middleware implementation

--- 03-back-end/src/components/account/router.ts
+++ 03-back-end/src/components/account/router.ts
@@ -3,16 +3,17 @@ import IApplicationResources from "../../common/IApplicationResources.interface"
 import IRouter from "../../common/IRouter.interface";
 import AccountService from "./service";
 import AccountController from "./controller";
+import AuthMiddleware from "../../middleware/auth.middleware";
 
 export default class AccountRouter implements IRouter{
     public setupRoutes(application: express.Application, resources: IApplicationResources){
         
         const accountController = new AccountController(resources);
 
-        application.get("/account/:id",        accountController.getById.bind(accountController));
-        application.get("/user/:uid/account",  accountController.getAllFromUser.bind(accountController));
-        application.post("/account",           accountController.add.bind(accountController));
-        application.put("/account/:id",        accountController.edit.bind(accountController));
-        application.delete("/account/:id",     accountController.deleteById.bind(accountController));
+        application.get("/account/:id",       AuthMiddleware.verifyAuthToken, accountController.getById.bind(accountController));
+        application.get("/user/:uid/account", AuthMiddleware.verifyAuthToken, accountController.getAllFromUser.bind(accountController));
+        application.post("/account",          AuthMiddleware.verifyAuthToken, accountController.add.bind(accountController));
+        application.put("/account/:id",       AuthMiddleware.verifyAuthToken, accountController.edit.bind(accountController));
+        application.delete("/account/:id",    AuthMiddleware.verifyAuthToken, accountController.deleteById.bind(accountController));
     }
 }
