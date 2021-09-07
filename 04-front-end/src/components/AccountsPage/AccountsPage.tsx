9ce1dd5ec84a8293c88bd52e8b96a3b643b36619
btzzar <luka.m.banovic@gmail.com>
2021-09-07 17:39:12 +0200

    Update component to match routes to parameters

--- 04-front-end/src/components/AccountsPage/AccountsPage.tsx
+++ 04-front-end/src/components/AccountsPage/AccountsPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import BasePage, { BasePageProperties } from "../BaseComponent/BaseComponent";
 
 class AccountsProperties extends BasePageProperties{
@@ -9,13 +10,87 @@ class AccountsProperties extends BasePageProperties{
     }
 }
 
+class AccountsState{
+    title: string = "Loading...";
+    transactions: any[] = [];
+}
+
 export default class AccountsPage extends BasePage<AccountsProperties> {
+
+    state: AccountsState;
+
+    constructor(props: AccountsProperties){
+        super(props);
+
+        this.state = {
+            title: "",
+            transactions: []
+        }
+    }
+
+    private getAccountData(){
+        const id = this.getAccountId();
+
+        if(id === null){
+            this.setState({
+                title: "All accounts",
+                transactions: []
+            });
+        }else{
+            this.setState({
+                title: "Transactions for account with id " + id,
+                transactions: [
+                    "1",
+                    "2",
+                    "3",
+                    "4"
+                ]
+            });
+        }
+
+        //set title and 
+    }
+
+    private getAccountId(): number|null {
+        const id = this.props.match?.params.id;
+
+        return id ? +(id) : null;
+    }
+
+    componentDidMount(){
+        this.getAccountData();
+    }
+
+    componentDidUpdate(prevProps: AccountsProperties, prevState: AccountsState){
+        if(prevProps.match?.params.id !== this.props.match?.params.id){
+            this.getAccountData();
+        }
+    }
 
     renderMain(): JSX.Element  {
 
         return (
 
-            <p>Ovo je kasa sa ID brojem: {this.props.match?.params.id }</p>
+            <>
+            
+                <h1> {this.state.title} </h1>
+
+                <p>Transakcije</p>
+                <ul>
+                    {
+                        this.state.transactions.map(
+                            tr => (
+                                <li>
+                                    <Link to= {"/accounts/" + tr}>
+                                        Transakcija {tr}
+                                    </Link>
+                                </li>
+                            )
+                        )
+                    }   
+                </ul>
+           
+            </>
 
         );
     }
