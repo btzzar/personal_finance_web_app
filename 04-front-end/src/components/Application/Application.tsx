3bbaa6c667de4fd97097852226c87975724e5ca8
btzzar <luka.m.banovic@gmail.com>
2021-09-08 17:40:34 +0200

    Fix naming

--- 04-front-end/src/components/Application/Application.tsx
+++ 04-front-end/src/components/Application/Application.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import AccountsPage from '../AccountsPage/AccountsPage';
+import AccountsPage from '../AccountPage/AccountPage';
 import HomePage from '../HomePage/HomePage';
 import TopMenu from '../TopMenu/TopMenu';
 import './Application.sass';
